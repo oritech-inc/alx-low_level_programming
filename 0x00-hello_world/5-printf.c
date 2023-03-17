98ad0206608dbb09a13828ea027e37b020461738
oritech-inc <origintech.inc@gmail.com>
2023-03-16 22:56:49 -0700

    Hello world programming with c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Aprogram that prints a line with pritf function
+ *
+ * Return: 0 (Sucess)
+ */
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+                return (0);
+}

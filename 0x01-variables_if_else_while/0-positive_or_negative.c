1d6c0a7108e96e91c17fed0fa12b1267aaf1d179
oritech-inc <origintech.inc@gmail.com>
2023-03-22 00:27:52 -0700

    This is my first commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,8 +9,10 @@
 int main(void)
 {
 	int n;
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
@@ -23,5 +25,6 @@ int main(void)
 	{
 		printf("%d is negative\n", n);
 	}
+	
 	return (0);
 }

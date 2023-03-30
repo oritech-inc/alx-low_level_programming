0b5af0d778e701b445175649cae90e04debface9
oritech-inc <origintech.inc@gmail.com>
2023-03-30 05:26:35 -0700

    Fixed all the files

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,45 +1,46 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
-* print_buffer - prints buffer
-* @b: buffer
-* @size: size
-* Return: void
-*/
+ * print_buffer - Prints a buffer 10 bytes at a time, starting with
+ *                the byte position, then showing the hex content,
+ *                then displaying printable charcaters.
+ * @b: The buffer to be printed.
+ * @size: The number of bytes to be printed from the buffer.
+ * Return: int
+ */
+
 void print_buffer(char *b, int size)
 {
-int o, j, i;
-o = 0;
-if (size <= 0)
-{
-printf("\n");
-return;
-}
-while (o < size)
-{
-j = size - o < 10 ? size - o : 10;
-printf("%08x: ", o);
-for (i = 0; i < 10; i++)
-{
-if (i < j)
-printf("%02x", *(b + o + i));
-else
-printf(" ");
-if (i % 2)
-{
-printf(" ");
-}
-}
-for (i = 0; i < j; i++)
-{
-int c = *(b + o + i);
-if (c < 32 || c > 132)
-{
-c = '.';
-}
-printf("%c", c);
-}
-printf("\n");
-o += 10;
-}
+	int bytes, index;
+
+	for (bytes = 0; bytes < size; bytes += 10)
+	{
+		printf("%08x: ", bytes);
+
+		for (index = 0; index < 10; index++)
+		{
+			if ((index + bytes) >= size)
+				printf("  ");
+			else
+				printf("%02x", *(b + index + bytes));
+			if ((index % 2) != 0 && index != 0)
+				printf(" ");
+		}
+		for (index = 0; index < 10; index++)
+		{
+			if ((index + bytes) >= size)
+				break;
+			else if (*(b + index + bytes) >= 31 &&
+				 *(b + index + bytes) <= 126)
+				printf("%c", *(b + index + bytes));
+			else
+				printf(".");
+		}
+		if (bytes >= size)
+			continue;
+		printf("\n");
+	}
+	if (size <= 0)
+		printf("\n");
 }

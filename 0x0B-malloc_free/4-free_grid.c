96cd5ed79a76ef6357caa2f264fd693432159034
oritech-inc <origintech.inc@gmail.com>
2023-06-13 02:22:45 -0700

    fixed betty styling on --free_grid.c file

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -6,9 +6,8 @@
  * @grid: 2d grid
  * @height: height dimension of grid
  * Description: frees memory of grid
- * Return: nothing		
+ * Return: nothing
  **/
-		
 void free_grid(int **grid, int height)
 {
 	int i;
